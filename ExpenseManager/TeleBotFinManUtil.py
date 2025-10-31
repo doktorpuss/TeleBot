c6d2c48705f626f4b65f40d59c544d5bd2c2fb24
doktorpuss <lazylarrry2392003@gmail.com>
2025-10-31 16:39:11 +0700

    hotfix

--- ExpenseManager/TeleBotFinManUtil.py
+++ ExpenseManager/TeleBotFinManUtil.py
@@ -632,7 +632,7 @@ def make_history_table(history: pd.DataFrame):
 
     save_path = f"{HISTORY_TABLE_DIRECTORY}/history_{get_this_month()}.png"
     hti = Html2Image()
-    hti.screenshot(html_str=html_full, save_as=os.path.basename(save_path), output_path=HISTORY_TABLE_DIRECTORY)
+    hti.screenshot(html_str=html_full, save_as=os.path.basename(save_path))
     return save_path
 
 # Conversation states
