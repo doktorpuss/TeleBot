0055deb37a77ee6db5212fa1a3b8e79db6537620
doktorpuss <lazylarrry2392003@gmail.com>
2026-01-08 19:19:06 +0700

    fix TeleBotFinManUtil add budget

--- ExpenseManager/TeleBotFinManUtil.py
+++ ExpenseManager/TeleBotFinManUtil.py
@@ -1016,7 +1016,9 @@ async def add_budget_entry(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return ConversationHandler.END
 
     # Get user id
-    user_id = crud.get_user_info(session, user_tele_id=user_tele_id).user_id
+    user_id = crud.get_user_info(session, user_tele_id=user_tele_id)
+    print(user_id)
+    user_id = user_id.user_id
     # user_id = 1 # For development
 
     # Get budget list
